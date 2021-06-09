b1e7328d94ed7990837c77e31d127332f802b0ce
reshmaattar <attar.reshma@gmail.com>
2021-06-09 13:18:50 +0100

    updated s3

--- dev/s3/main.tf
+++ dev/s3/main.tf
@@ -1,22 +1,14 @@
 provider "aws" {
-  region  = "eu-west-1"
-  profile = "circleci_sandbox"
+  region = "eu-west-1"
+
 }
 
 terraform {
-  backend "s3" {
-    bucket         = "terraform-mochi-state"
-    key            = "dev/s3/terraform.tfstate"
-    profile        = "circleci_sandbox"
-    region         = "eu-west-1"
-    dynamodb_table = "terraform-state-locking"
-    encrypt        = true
-
-  }
+  backend "s3" {}
 }
 
 module "s3" {
   source    = "../../modules/s3"
   infra_env = var.infra_env
-  
+
 }
--- dev/s3/outputs.tf
+++ dev/s3/outputs.tf
@@ -1,4 +1,4 @@
 output "mochi_telemetry_bucket_arn" {
-    value = module.s3.mochi_telemetry_bucket_arn
-  
+  value = module.s3.mochi_telemetry_bucket_arn
+
 }
