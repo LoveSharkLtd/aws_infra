0c2517174047e1e325396970338b066839528889
reshmaattar <attar.reshma@gmail.com>
2021-06-09 12:53:16 +0100

    updated kinesis with terragrunt

--- dev/kinesis/main.tf
+++ dev/kinesis/main.tf
@@ -1,35 +1,27 @@
 provider "aws" {
-  region  = "eu-west-1"
-  profile = "circleci_sandbox"
+  region = "eu-west-1"
 }
 
 terraform {
-  backend "s3" {
-    bucket         = "terraform-mochi-state"
-    key            = "dev/kinesis/terraform.tfstate"
-    profile        = "circleci_sandbox"
-    region         = "eu-west-1"
-    dynamodb_table = "terraform-state-locking"
-    encrypt        = true
+  backend "s3" {}
 
-  }
 }
 
 module "kinesis" {
   source = "../../modules/kinesis"
 
-  stream_name      = var.stream_name
+  stream_name          = var.stream_name
   telemetry_bucket_arn = data.terraform_remote_state.s3.outputs.mochi_telemetry_bucket_arn
-  kinesis_shard_count = var.kinesis_shard_count
+  kinesis_shard_count  = var.kinesis_shard_count
 }
 
 
 data "terraform_remote_state" "s3" {
   backend = "s3"
   config = {
-    bucket               = "terraform-mochi-state"
-    key                  = "dev/s3/terraform.tfstate"
-    profile        = "circleci_sandbox"
-    region         = "eu-west-1"
+    bucket  = "terraform-mochi-state"
+    key     = "${var.infra_env}/s3/terraform.tfstate"
+    profile = "circleci_sandbox"
+    region  = "eu-west-1"
   }
 }
