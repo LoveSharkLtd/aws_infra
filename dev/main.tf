ed28aef2b5b0cc460703964171cb4099a6975d97
reshmaattar <attar.reshma@gmail.com>
2021-06-09 13:38:30 +0100

    added backend on root of environment

--- dev/main.tf
+++ dev/main.tf
@@ -1,10 +1,16 @@
 provider "aws" {
-  region  = "eu-west-1"
-  profile = "circleci_sandbox"
+  region = "eu-west-1"
 }
 
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket         = "terraform-mochi-state"
+    key            = "dev/terraform.tfstate"
+    region         = "eu-west-1"
+    dynamodb_table = "terraform-state-locking"
+    encrypt        = true
+
+  }
   # backend "s3" {
   #   bucket = "terraform-mochi-state"
   #   key = "dev/terraform.tfstate"
