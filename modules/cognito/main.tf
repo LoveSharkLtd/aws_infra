ace5f538c88ff21e7574147d48dafdc67648ec32
reshmaattar <attar.reshma@gmail.com>
2021-07-06 18:26:53 +0100

    added cms cognito

--- modules/cognito/main.tf
+++ modules/cognito/main.tf
@@ -162,3 +162,70 @@ resource "aws_ssm_parameter" "cognito_app_client_id" {
   type        = "String"
   value       = aws_cognito_user_pool_client.mochi_ios_client.id
 }
+
+
+######## cms cognito pool 
+
+resource "aws_cognito_user_pool" "cms-userpool" {
+
+  name                       = "cms-${var.infra_env}"
+  username_attributes        = ["email", "phone_number"]
+  auto_verified_attributes   = ["email"]
+  mfa_configuration          = "OFF"
+  
+  account_recovery_setting {
+    recovery_mechanism {
+      name     = "verified_email"
+      priority = 1
+    }
+    recovery_mechanism {
+      name     = "verified_phone_number"
+      priority = 2
+    }
+
+  }
+  admin_create_user_config {
+    allow_admin_create_user_only = true
+    invite_message_template {
+      email_message = "Your username is {username} and temporary password is {####}. "
+      email_subject = "${var.infra_env}:Your temporary password"
+      sms_message   = "Your username is {username} and temporary password is {####}. "
+    }
+
+  }
+
+  password_policy {
+    minimum_length                   = 8
+    require_lowercase                = true
+    require_numbers                  = true
+    require_symbols                  = false
+    require_uppercase                = false
+    temporary_password_validity_days = 7
+  }
+
+  schema {
+    attribute_data_type          = "String"
+    developer_only_attribute     = false
+    mutable                      = true
+    name                         = "email"
+    required                     = true
+    string_attribute_constraints {
+      max_length = "2048"
+      min_length = "0"
+    }
+
+  }
+  schema {
+    attribute_data_type          = "String"
+    developer_only_attribute     = false
+    mutable                      = true
+    name                         = "preferred_username"
+    required                     = true
+    string_attribute_constraints {
+      max_length = "2048"
+      min_length = "0"
+    }
+
+  }
+
+}
