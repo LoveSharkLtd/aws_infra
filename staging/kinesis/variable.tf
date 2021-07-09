b504d57447d8320b792c12c02704c98bab122052
reshmaattar <attar.reshma@gmail.com>
2021-07-09 13:57:36 +0100

    removed empty output and updated description

--- staging/kinesis/variable.tf
+++ staging/kinesis/variable.tf
@@ -1,12 +1,12 @@
 variable "stream_name" {
   type        = string
-  description = "stream_name environment"
+  description = "kinesis stream_name"
 
 }
 
 variable "kinesis_shard_count" {
   type        = string
-  description = "kinesis_shard_count environment"
+  description = "kinesis_shard_count number of shard count required for kinesis data stream"
 
 }
 
--- staging/sns/variable.tf
+++ staging/sns/variable.tf
@@ -1,5 +1,5 @@
 variable "sns_platform_application" {
   type        = string
-  description = "sns_platform_application environment"
+  description = "sns_platform_application is an platform application name"
 
 }
