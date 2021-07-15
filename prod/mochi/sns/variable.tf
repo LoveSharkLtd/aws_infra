bc8c3fac94e40b9cf8775466ae296bc01a115645
reshmaattar <attar.reshma@gmail.com>
2021-07-15 12:19:37 +0100

    Update prod/mochi/sns/variable.tf
    Co-authored-by: sudhir-loveshark <83642271+sudhir-loveshark@users.noreply.github.com>

--- prod/mochi/sns/variable.tf
+++ prod/mochi/sns/variable.tf
@@ -1,5 +1,5 @@
 variable "sns_platform_application" {
   type        = string
-  description = "sns_platform_application is an platform application name"
+  description = "sns platform application name"
 
 }
