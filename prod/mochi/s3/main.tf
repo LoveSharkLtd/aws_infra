1194d591477d365ad6413b921d07d23ff192dff4
reshmaattar <attar.reshma@gmail.com>
2021-07-26 19:22:39 +0100

    updated prod s3

--- prod/mochi/s3/main.tf
+++ prod/mochi/s3/main.tf
@@ -3,12 +3,90 @@ provider "aws" {
 
 }
 
+provider "aws" {
+  alias  = "eu-west-2"
+  region = "eu-west-2"
+}
+
 terraform {
   backend "s3" {}
 }
 
-module "s3" {
-  source    = "../../../modules/mochi/s3"
-  infra_env = var.infra_env
+resource "aws_s3_bucket" "test_bucket" {
+  bucket        = "delete-movearchive"
+  provider      = aws.eu-west-2
+  acl           = "private"
+  force_destroy = false
+
+
+
+  lifecycle {
+
+    prevent_destroy = true
+  }
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "PUT", "POST"]
+    allowed_origins = ["*"]
+    expose_headers  = []
+    max_age_seconds = 3000
+  }
+}
+
+resource "aws_s3_bucket" "mochi_bucket" {
+  bucket        = "loveshark-prod"
+  provider      = aws.eu-west-2
+  force_destroy = false
+
+  lifecycle {
+
+    prevent_destroy = true
+  }
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "POST", "PUT", ]
+    allowed_origins = ["*"]
+    expose_headers  = []
+    max_age_seconds = 3000
+  }
+}
+
+resource "aws_ssm_parameter" "mochi_assets_bucket" {
+  name        = "mochi_assets_bucket"
+  description = "mochi_assets_bucket "
+  type        = "String"
+  overwrite   = true
+  value       = aws_s3_bucket.mochi_bucket.bucket
+}
+
+
+resource "aws_s3_bucket" "mochi_telemetry_bucket" {
+  bucket        = "mochi-telemetry-data"
+  provider      = aws.eu-west-2
+  force_destroy = false
+  grant {
+    id          = "abcd6fea6710c0a2ecccf11af3cce02f2a8d7565571fd18ad24def693bf0aa64"
+    permissions = ["READ", "READ_ACP", "WRITE", "WRITE_ACP"]
+    type        = "CanonicalUser"
+  }
+  lifecycle {
+
+    prevent_destroy = true
+  }
+}
+
+resource "aws_ssm_parameter" "mochi_telemetry_bucket" {
+  name        = "mochi_telemetry_bucket"
+  description = "mochi_telemetry_bucket "
+  type        = "String"
+  overwrite   = true
+  value       = aws_s3_bucket.mochi_telemetry_bucket.bucket
+}
+
+
+output "mochi_telemetry_bucket_arn" {
+  value = aws_s3_bucket.mochi_telemetry_bucket.arn
 
 }
--- prod/mochi/s3/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "mochi_telemetry_bucket_arn" {
-  value = module.s3.mochi_telemetry_bucket_arn
-
-}
