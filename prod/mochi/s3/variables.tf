3b7e5ac2c1f82727006e23dcf2ee26f7be2857f6
reshmaattar <attar.reshma@gmail.com>
2021-07-27 13:03:31 +0100

    removed unnecessary variable

--- prod/mochi/s3/variables.tf
+++ prod/mochi/s3/variables.tf
@@ -4,26 +4,7 @@ variable "infra_env" {
 
 }
 
-variable "mochi_asset_bucket" {
-  type        = string
-  default     = "loveshark-prod"
-  description = "bucket_name for mochi asset"
 
-}
-
-variable "telemetry_bucket" {
-  type        = string
-  default     = "mochi-telemetry-data"
-  description = "telemetry_bucket for mochi asset"
-
-}
-
-variable "mochi_asset_bucket_region" {
-  type        = string
-  default     = "eu-west-1"
-  description = "region for mochi bucket"
-
-}
 
 
 
